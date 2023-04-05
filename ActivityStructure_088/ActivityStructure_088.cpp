f5cd4ae2c368aff7fcdcd5c478c9ba37c3b8001e
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-04-05 10:50:35 +0700

    memasukan variabel nama dan data mahasiswa dan menampilkannya ke dalam program

--- ActivityStructure_088/ActivityStructure_088.cpp
+++ ActivityStructure_088/ActivityStructure_088.cpp
@@ -8,3 +8,31 @@ struct Mahasiswa {
 	int umur;
 };
 
+int main() {
+	Mahasiswa mhs, mhs2;
+
+	mhs.nim = "20220140088";
+	mhs.umur = 20;
+	mhs.nama = "Muhamad Fathur";
+	mhs.alamat = "Wonosobo";
+
+	cout << "Masukan NIM : ";
+	cin >> mhs2.nim;
+	cout << "Masukan Umur : ";
+	cin >> mhs2.umur;
+	cout << "Masukan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat : ";
+	cin >> mhs2.alamat;
+
+	cout << "NIM : " << mhs.nim;
+	cout << "\nUmur : " << mhs.umur;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\Alamat : " << mhs.alamat;
+
+	cout << "NIM : " << mhs2.nim;
+	cout << "\nUmur : " << mhs2.umur;
+	cout << "\nNama : " << mhs2.nama;
+	cout << "\Alamat : " << mhs2.alamat;
+	
+}
