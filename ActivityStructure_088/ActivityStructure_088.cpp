5aec0832c68471087b9b8a7a88db3776b8c249d8
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-04-05 11:47:15 +0700

    memeperbaiki kesalahan pada program latihan ke satu

--- ActivityStructure_088/ActivityStructure_088.cpp
+++ ActivityStructure_088/ActivityStructure_088.cpp
@@ -25,14 +25,14 @@ int main() {
 	cout << "Masukan Alamat : ";
 	cin >> mhs2.alamat;
 
-	cout << "NIM : " << mhs.nim;
+	cout << "\n\nNIM : " << mhs.nim;
 	cout << "\nUmur : " << mhs.umur;
 	cout << "\nNama : " << mhs.nama;
 	cout << "\nAlamat : " << mhs.alamat;
 
 	cout << "\n\nNIM : " << mhs2.nim;
-	cout << "\n\nUmur : " << mhs2.umur;
-	cout << "\n\nNama : " << mhs2.nama;
-	cout << "\n\nAlamat : " << mhs2.alamat;
+	cout << "\nUmur : " << mhs2.umur;
+	cout << "\nNama : " << mhs2.nama;
+	cout << "\nAlamat : " << mhs2.alamat;
 	
 }
